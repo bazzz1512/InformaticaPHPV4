b9f24bb6f81b2fd05867b68db11178fcedc31afe
bazzz1512 <bazzz1512@users.noreply.github.com>
2017-06-16 10:22:04 +0200

    Add files via upload

---
+++ inlogverwerking.php
@@ -0,0 +1,28 @@
+﻿<?php
+session_start();
+ if (!isset($_SESSION['ingelogd'])) {
+    $_SESSION['ingelogd'] = false;
+	}
+    if (isset($_POST['wachtwoord'])) {
+    $sWachtwoord = $_POST['wachtwoord'];
+	}
+
+if (isset($_POST['gebruikersnaam'])) {
+    $sGebruikersnaam = $_POST['gebruikersnaam'];
+}
+//	$_SESSION['gebruikersnaam']= $_POST['gebruikersnaam'];
+//	$_SESSION['wachtwoord']= $_POST['wachtwoord'];
+	if($sWachtwoord == 'informatica')
+		{
+			$_SESSION['ingelogd']= true;
+			header ('Location: inde.php');	
+			
+		}
+		else
+		{
+			header ('Location: inlog.php');
+			$_SESSION['ingelogd']= false;
+		}
+
+?>
+
